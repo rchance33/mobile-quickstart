9aeda9de25be5433eefcbc0afcd1d61593c0afb4
richardscottchance <richardscottchance@yahoo.com>
2015-05-11 04:36:59 +0800

    no message

--- auth.php
+++ auth.php
@@ -14,7 +14,7 @@ if($_REQUEST['allowOutgoing'] != 'false')
     $capability->allowClientOutgoing($appSid);
 
 if($_REQUEST['client'] != null)
-    $capability->allowClientIncoming($appSid);
+    $capability->allowClientIncoming('mobile-quickstart4');
 // This would return a token to use with Twilio based on // the account and capabilities defined above 
 $token = $capability->generateToken();
 
