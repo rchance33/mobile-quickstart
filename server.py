94d68293aca4cc6a77313880b31f7da70b3f8e77
richardscottchance <richardscottchance@yahoo.com>
2015-05-11 02:04:45 +0800

    no message

--- server.py
+++ server.py
@@ -54,15 +54,22 @@ def call():
   if not from_client:
     # PSTN -> client
     # resp.dial(callerId=from_value).client(CLIENT)
-    resp.dial(callerId=from_value).conference(CLIENT)
+    # resp.dial(callerId=from_value).conference(CLIENT)
+    with dial(callerId=from_value) as g:
+        g.conference(CLIENT)
+
   elif to.startswith("client:"):
     # client -> client
     # resp.dial(callerId=from_value).client(to[7:])
-    resp.dial(callerId=from_value).conference(to[7:])
+    # resp.dial(callerId=from_value).conference(to[7:])
+    with dial(callerId=from_value) as g:
+        g.conference(to[7:])
   else:
     # client -> PSTN
     # resp.dial(to, callerId=caller_id)
-    resp.dial(to, callerId=caller_id).conference(to)
+    # resp.dial(to, callerId=caller_id).conference(to)
+    with dial(callerId=from_value) as g:
+        g.conference(to)
   return str(resp)      
 
 #@app.route('/mute', methods=['GET', 'POST'])
