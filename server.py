88d10ab93906c1933e76235b6b12e736e32d2555
richardscottchance <richardscottchance@yahoo.com>
2015-05-11 08:44:13 +0800

    no message

---
+++ server.py
@@ -0,0 +1,110 @@
+import os
+from flask import Flask, request
+from twilio.util import TwilioCapability
+import twilio.twiml
+from twilio.rest import TwilioRestClient
+##
+# Account Sid and Auth Token can be found in your account dashboard
+ACCOUNT_SID = 'AC4cbe7e3cb0b70ab1eb2e5ccf81005c71'
+AUTH_TOKEN = 'b0ccc8c0fcb902a9531482c9e8911fac'
+
+# TwiML app outgoing connections will use
+APP_SID = 'APb7372771a18a38669a83e5f115dabeb8'
+
+CALLER_ID = '+19189924892'
+CLIENT = 'mobile-quickstart4'
+
+app = Flask(__name__)
+
+@app.route('/token')
+def token():
+  account_sid = os.environ.get("ACCOUNT_SID", ACCOUNT_SID)
+  auth_token = os.environ.get("AUTH_TOKEN", AUTH_TOKEN)
+  app_sid = os.environ.get("APP_SID", APP_SID)
+
+  capability = TwilioCapability(account_sid, auth_token)
+
+  # This allows outgoing connections to TwiML application
+  if request.values.get('allowOutgoing') != 'false':
+     capability.allow_client_outgoing(app_sid)
+
+  # This allows incoming connections to client (if specified)
+  client = request.values.get('client')
+  if client != None:
+    capability.allow_client_incoming("mobile-quickstart4")
+
+  # This returns a token to use with Twilio based on the account and capabilities defined above
+  return capability.generate()
+
+@app.route('/call', methods=['GET', 'POST'])
+def call():
+  """ This method routes calls from/to client                  """
+  """ Rules: 1. From can be either client:name or PSTN number  """
+  """        2. To value specifies target. When call is coming """
+  """           from PSTN, To value is ignored and call is     """
+  """           routed to client named CLIENT                  """
+  resp = twilio.twiml.Response()
+  from_value = request.values.get('From')
+  to = request.values.get('To')
+  if not (from_value and to):
+    return str(resp.say("Invalid request"))
+  from_client = from_value.startswith('client')
+  caller_id = os.environ.get("CALLER_ID", CALLER_ID)
+  
+  if not from_client:
+    # PSTN -> client
+    # resp.dial(callerId=from_value).client(CLIENT)
+    # resp.dial(callerId=from_value).conference(CLIENT)
+    with resp.dial(callerId=from_value).client(CLIENT) as g:
+       g.conference("Room1")
+
+  elif to.startswith("client:"):
+    # client -> client
+    resp.dial(callerId=from_value).client(to[7:])
+    #with resp.dial(callerId=from_value).client(to[7:]) as g:
+    #   g.conference("Room1")
+  else:
+    # client -> PSTN
+    resp.dial(to, callerId=caller_id)
+    # resp.dial(to, callerId=caller_id).conference(to)
+    # with resp.dial(to, callerId=caller_id) as g:
+    #   g.conference("Room1")
+  return str(resp)      
+
+#@app.route('/mute', methods=['GET', 'POST'])
+#def mute():
+#  resp = twilio.twiml.Response()
+#  from_value = request.values.get('From')
+#  to = request.values.get('To')
+#  if not (from_value and to):
+#    return str(resp.say("Invalid request"))
+#  from_client = from_value.startswith('client')
+#  caller_id = os.environ.get("CALLER_ID", CALLER_ID)
+#  mute = request.values.get("Mute")
+  
+#  if mute == 'Yes':
+#    if not from_client:
+#      resp.dial(callerId=from_value).conference(CLIENT, muted='true')  
+#    elif to.startswith("client:"): 
+#      resp.dial(callerId=from_value).conference(to[7:], muted='true')
+#    else:
+#      resp.dial.conference(to, callerId=caller_id, muted='true')
+#  else:
+#    if not from_client:
+#     resp.dial(callerId=from_value).conference(CLIENT, muted='false')  
+#    elif to.startswith("client:"): 
+#      resp.dial(callerId=from_value).conference(to[7:], muted='false')
+#    else:
+#      resp.dial.conference(to, callerId=caller_id, muted='false')
+       
+#  return str(resp)
+  
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+  resp = twilio.twiml.Response()
+  resp.say("Welcome to Twilio")
+  return str(resp)
+
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
