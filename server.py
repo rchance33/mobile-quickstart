5b48c35cd74af99f49fb2f4ae380d22288618aa9
rchance33 <richardscottchance@yahoo.com>
2015-07-06 14:09:26 -0500

    Update server.py

--- server.py
+++ server.py
@@ -71,9 +71,9 @@ def music():
     response.say("A representitive will be with you shortly")
     response.play("https://s3.amazonaws.com/hotcoffeydesign/Uptown+Funk+Feat+Bruno+Mars+Mark+Ronson+-+1420814645+Part+1+of+5.mp3")
     #TODO: sms notifacation when someone calls
-    #client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-    #call = client.calls.create(url="http://mobile-quickstart4.herokuapp.com/agent",to="+19189924892",from_="+19189472310") 
-    #print call.sid
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+    call = client.calls.create(url="http://mobile-quickstart4.herokuapp.com/agent",to="+19189924892",from_="+19189472310") 
+    print call.sid
     #with response.gather(numDigits=1, action="/digit", method="POST") as g:
         #g.say("To continue the call, press 1. To leave a message, press 2.")
     return str(response)
