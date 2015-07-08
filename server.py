1c4888cb97162f338b3a1867df9efee83f222398
rchance33 <richardscottchance@yahoo.com>
2015-07-08 13:39:51 -0500

    Update server.py

--- server.py
+++ server.py
@@ -70,7 +70,7 @@ def ring():
     response = twilio.twiml.Response()
     #Use Enqueue verb to place caller in a Queue
     response.enqueue("hold queue",waitUrl="/music")
-  #We are going to put some code in here to see if it initiates to update our call to the hold queue
+    #We are going to put some code in here to see if it initiates to update our call to the hold queue
     member = client.members('/caller').dequeue("https://mobile-quickstart-quick.herokuapp.com/hold","Front",method="POST")
     print member.wait_time
     
